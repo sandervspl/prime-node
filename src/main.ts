764982e2667299c2d42d93cb94caa525fcf47450
sandervspl <sandervispoel@hotmail.com>
2021-10-06 11:49:50 +0200

    Add lint config

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
+import { AddressInfo } from 'net';
 import dotenv from 'dotenv';
 import express from 'express';
-import { AddressInfo } from 'net';
 
 dotenv.config();
 
@@ -8,7 +8,7 @@ const app = express();
 
 const listener = app.listen(Number(process.env.PORT), () => {
   const { port } = listener.address() as AddressInfo;
-  console.log(`Node server started on port ${port}`);
+  console.info(`Node server started on port ${port}`);
 
   app.get('/', ({ res }) => {
     res.status(200).send('OK');
